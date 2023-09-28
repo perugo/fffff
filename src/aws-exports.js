b74003ee99efbe237f88d5e7229567975def684a
girard-noel <girardperugo2000@gmail.com>
2023-09-28 23:59:03 +0900

    fafaf

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,7 @@ const awsmobile = {
     "aws_user_pools_id": "ap-northeast-1_FKtwWIFfv",
     "aws_user_pools_web_client_id": "4j2gq77jdm80clcvtip8fb4330",
     "oauth": {
-        "domain": "auth.simulationwave.com",
+        "domain": "https://simulationwave.auth.ap-northeast-1.amazoncognito.com",
         "scope": [
             "aws.cognito.signin.user.admin",
             "email",

169e6d9e96d60753575988856b2096d682724265
girard-noel <girardperugo2000@gmail.com>
2023-09-28 23:41:47 +0900

    fafaf

--- src/App.js
+++ src/App.js
@@ -8,15 +8,24 @@ Amplify.configure(awsConfig);
 function App() {
   const [user, setUser] = useState(null);
   const [customState, setCustomState] = useState(null);
+  const [email, setEmail] = useState(null);
+
 
   useEffect(() => {
     const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+      const fetchEmail = async () => { // 非同期関数を定義
+        const userInfo = await Auth.currentAuthenticatedUser();
+        setEmail(userInfo.attributes.email);
+      };
       switch (event) {
         case "signIn":
           setUser(data);
+          fetchEmail();  // そして、ここでその非同期関数を呼び出します
+
           break;
         case "signOut":
           setUser(null);
+          setEmail(null);
           break;
         case "customOAuthState":
           setCustomState(data);
@@ -24,9 +33,11 @@ function App() {
     });
 
     Auth.currentAuthenticatedUser()
-      .then(currentUser => setUser(currentUser))
-      .catch(() => console.log("Not signed in"));
-
+    .then(currentUser => {
+      setUser(currentUser);
+      setEmail(currentUser.attributes.email);  // ここでもメールアドレスを取得して設定します
+    })
+    .catch(() => console.log("Not signed in"));
     return unsubscribe;
   }, []);
 
@@ -36,6 +47,8 @@ function App() {
       <button onClick={() => Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google })}>Open Google</button>
       <button onClick={() => Auth.signOut()}>Sign Out</button>
       <div>{user && user.getUsername()}</div>
+      <div>Email: {email}</div> {/* ここでメールアドレスを表示します */}
+
     </div>
   );
 }
